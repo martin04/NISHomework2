5e6e80d352e07535f759c654a943614a2edf97e9
Martin <mr@medialab.io>
2016-12-25 14:22:45 +0100

    fixes in main

--- src/main/HomeworkMain.java
+++ src/main/HomeworkMain.java
@@ -7,61 +7,43 @@ import math.OperationAdd;
 import math.OperationDivide;
 import math.OperationMultiply;
 import math.OperationSubstract;
+import observer.Subject;
 
 public class HomeworkMain {
 	public static boolean newCommand;
 	public static String command;
-
-	public static void produce() {
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				Scanner scanner = new Scanner(System.in);
-				while (true) {
-					System.out.print("Set_val[A] Set_val[B] [+]AddObserver [-]RemoveObserver e[X]it >");
-					command = scanner.nextLine();
-					newCommand = true;
-				}
-			}
-		}).start();
-
-	}
-
-	public static void consume() {
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				while (true) {
-					if (newCommand) {
-						switch (command) {
-						case "+":
-							break;
-						case "-":
-							break;
-						case "A":
-							break;
-						case "B":
-							break;
-						case "X":
-							System.exit(0);
-							break;
-						default:
-							break;
-						}
-					}
-				}
-			}
-		}).start();
-	}
+	private static Subject subjectA;
+	private static Subject subjectB;
+	private static Scanner scanner;
 
 	public static void main(String[] args) {
+		scanner = new Scanner(System.in);
+		subjectA = new Subject();
+		subjectA.setState(10);
 
-		float state1 = 10;
-		float state2 = 5;
-		
-		produce();
-		consume();
+		subjectB = new Subject();
+		subjectB.setState(5);
+
+		while (true) {
+			System.out.print("Set_val[A] Set_val[B] [+]AddObserver [-]RemoveObserver e[X]it >");
+			command = scanner.nextLine();
+			if (command.equals("+")) {
+				System.out.print("\nSet New Observer (A|B)(+|-|*|/) <num>):");
+			} else if (command.equals("-")) {
+				System.out.print("\nSet New Observer (A|B)(+|-|*|/) <num>):");
+			} else if (command.equals("A")) {
+				Double newValue = null;
+				System.out.print("Value = ");
+				newValue = scanner.nextDouble();
+				subjectA.setState(newValue);
+			} else if (command.equals("B")) {
+				Double newValueB = null;
+				System.out.print("Value = ");
+				newValueB = scanner.nextDouble();
+				subjectB.setState(newValueB);
+			} else if (command.equals("X")) {
+				System.exit(0);
+			}
+		}
 	}
 }
